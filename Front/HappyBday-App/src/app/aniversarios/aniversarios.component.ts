42ef8c9817192aa3d994de6a71a1dcf98c84b34c
Jean Connory <michaelrhcp@gmail.com>
2022-02-11 22:53:20 -0300

    Listagem de aniversarios concluida igual a de parentesco

--- Front/HappyBday-App/src/app/aniversarios/aniversarios.component.ts
+++ Front/HappyBday-App/src/app/aniversarios/aniversarios.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AniversariosComponent implements OnInit {
 
-  constructor() { }
+  public aniversarios: any = [];
+  public aniversariosFiltrados: any = [];
+  larguraImg: number = 75;
+  margemImg: number = 2;
+  exibirImg: boolean = true;
+  private _filtroLista: string = '';
 
-  ngOnInit() {
+  public get filtroLista(): string {
+    return this._filtroLista;
   }
 
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.aniversariosFiltrados = this.filtroLista ? this.filtrarAniversarios(this.filtroLista) : this.aniversarios;
+  }
+
+  filtrarAniversarios(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.aniversarios.filter(
+      (niver: any) => niver.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+       niver.email.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getAniversarios();
+  }
+
+  alterarImagem() {
+    this.exibirImg = !this.exibirImg;
+  }
+
+  public getAniversarios():void {
+    this.http.get('https://localhost:5001/api/aniversarios').subscribe(
+      response => {
+        this.aniversarios = response,
+        this.aniversariosFiltrados = this.aniversarios
+      },
+      error => console.log(error)
+    );
+  }
 }
