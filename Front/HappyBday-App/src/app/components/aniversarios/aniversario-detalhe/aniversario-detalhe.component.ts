51eadb14ba6de992af789efbd514612d18ec7223
Jean Connory <michaelrhcp@gmail.com>
2022-02-20 00:22:33 -0300

    com as validações dos formularios e encerramento da secao 8

--- Front/HappyBday-App/src/app/components/aniversarios/aniversario-detalhe/aniversario-detalhe.component.ts
+++ Front/HappyBday-App/src/app/components/aniversarios/aniversario-detalhe/aniversario-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-aniversario-detalhe',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AniversarioDetalheComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  get f(): any {
+    return this.form.controls;
+  }
 
   ngOnInit(): void {
+    this.validation();
   }
+
+ public validation(): void {
+   this.form = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(150)]],
+    dataAniversario: ['', Validators.required],
+    telefone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    imagemUrl: ['', Validators.required]
+   });
+ }
+
+ public resetForm(): void {
+  this.form.reset();
+ }
 
 }
